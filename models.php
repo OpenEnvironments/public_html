37a4b841ac4c17e6e9a2a37ede8d675378a97dcb
michaelbbryan <michael.b.bryan@gmail.com>
2023-05-07 19:51:14 +0000

    New models page

--- models.php
+++ models.php
@@ -1,28 +1,35 @@
 <?php
 
-$GLOBALS["page_id"] = basename($_SERVER['PHP_SELF']);
 include "head.php";
 
 ?>
-
 <!--------- Content Area (a 3x3 table using outer cells for spacing) ----------->
 <table width="100%" class="OEcontent">
 	<tr height="10px"><td colspan="3"><td></tr>  <!--- top row spacing --->
-	<tr>
-		<td width="10px"></td> <!--- left side spacing --->
-		<td style="padding: 20px 20px 20px 80px;">
-		<table><tr>
-			<td width="5%"><img src="images/model.png" style="width=180px; height: 180px;"><br><br><br><br></td>
-			<td style="font-size: 18px; text-align: center;">
-				<br><br><b>A catalog of Models are still under construction.</b><br><br>( if you need anything, mail us at <a href="mailto:support@openenvironments.com?subject=Settings Request">support@openenvironments.com )</a> <br>
-				<br><br><br><br>
-			</td>
-		</tr></table>
+	<tr><td width="10px"></td>
+		<td>
+			<?php
+			$connstr = "host=" . $OEhost . " port=" . $OEport . " dbname=" . $OEname . " user=" . $OEuser . " password=" . $OEpass;
+			$conn = pg_connect($connstr);
+			$cursor = pg_query($conn, "select * from core.model");
+			while ($row = pg_fetch_assoc($cursor) ){
+			echo  "<a href=\"".$row['model_url']."\" target=\"_blank\">
+					<div class=\"OEcard\">
+					 	<div class=\"OEcard-publisher\">
+							<div>
+								<img src=\"publishers/".$row['model_image']."\" class=\"OEcard-publisher-image\">
+								</div>
+							<div class=\"OEcard-publisher-name\">".$row['model_name']."</div>
+							<div class=\"OEcard-publisher-description\">".$row['model_description']."</div>
+						</div>
+			     	</div></a>";
+			};
+			?>
 		</td>
-	<td style="padding: 20px 120px 20px 20px;"><!--- right side spacing ---></td>
+		<td></td>
 	</tr>
-	<tr height="10px"><td colspan="3"><td></tr>   <!--- bottom row spacing --->
-</td></tr></table>
+	<tr height="10px"><td colspan="3"><td></td></tr>   <!--- bottom row spacing --->
+</table>
 <?php
 
 include "foot.php"
